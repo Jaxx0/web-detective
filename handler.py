ba89d0682ad529e702893d560229a9648daf9a2b
Jackson <jackson.kitili@gmail.com>
2019-09-30 18:20:47 +0300

    Implemented a DynamoDB stream function that makes requests and updates information on the DB

--- handler.py
+++ handler.py
@@ -5,7 +5,7 @@ import boto3
 
 from files.document_processor import crawler
 from files.bucket_stores import create_file_name, save_to_s3, get_s3_object_url
-from files.database_stores import post_record, create_partition_key, save_to_db, get_URL_from_db, update_record
+from files.database_stores import post_record, create_partition_key, save_to_db, get_URL_from_db, update_record, query
 
 """This function receives a URL as an argument from the API gateway and passes it to the crawler function It then 
 stores the response to an s3 bucket an a DynamoDB and returns the extracted title and the S3 URL of the stored 
@@ -94,6 +94,32 @@ def get_url_given_identifier(event, context):
         s3_url = get_s3_object_url(os.environ['BUCKET_NAME'], file_name=identifier)
         response = update_record(identifier, s3_url, title, table=os.environ['URL_TABLE_NAME'])
         return dict(statusCode=200, body=json.dumps(response))
+
+    except Exception as e:
+        return dict(statusCode=200, body=str(e))
+
+
+""" This function gets called whenever the database table gets populated with a data record
+    It retrieves the identifier to retrieve the URL, makes a request to the URL to obtain a response and 
+    processes the response to extract the title and obtain the s3 URL of the response object and then
+                            updates the status to "PROCESSED" """
+
+
+def dynamo_trigger_stream(event, context):
+    try:
+        current_region = boto3.session.Session().region_name
+        db = boto3.resource('dynamodb', region_name=current_region)
+        table = db.Table(os.environ['URL_TABLE_NAME'])
+        response = table.scan()
+        for resp in response['Items']:
+            if resp['state'] == "PENDING":
+                id = resp['id']
+                url = resp['url']
+                response = crawler(url)
+                title = response['title']
+                s3_url = get_s3_object_url(os.environ['BUCKET_NAME'], file_name=id)
+                response = update_record(id, s3_url, title, table=os.environ['URL_TABLE_NAME'])
+                return dict(statusCode=200, body=json.dumps(response))
 
     except Exception as e:
         return dict(statusCode=200, body=str(e))
