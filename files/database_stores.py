48ec2f63a50120b976518be86e4e6d47df399e28
Jackson <jackson.kitili@gmail.com>
2019-09-30 13:01:57 +0300

    Refactored code to test locally by invoking the local function with a static url

--- files/database_stores.py
+++ files/database_stores.py
@@ -1,13 +1,13 @@
 import boto3
 
-""" This function receives the extracted title as an argument and stores it in the titles Table of DynamoDB"""
+""" This function receives the extracted title and table as an argument and stores it in the Table of DynamoDB"""
 
 
-def post_record(title):
+def post_record(title, table):
     current_region = boto3.session.Session().region_name
     db = boto3.resource('dynamodb', region_name=current_region)
 
-    table = db.Table('titles')
+    table = db.Table(table)
     try:
         response = table.put_item(
             Item={
--- handler.py
+++ handler.py
@@ -9,19 +9,22 @@ from files.bucket_stores import create_file_name, save_to_s3, get_s3_object_url
 
 
 def document_crawler(event, context):
-    if event['httpMethod'] == 'GET' and event['queryStringParameters']['query']:
-        url = event['queryStringParameters']['query']
+    # if event['httpMethod'] == 'GET' and event['queryStringParameters']['query']:
+    #     url = event['queryStringParameters']['query']
+        url = "https://www.netflix.com"
         body = crawler(url)
         response = {
             "statusCode": 200,
             "body": json.dumps(body)
         }
         # To Do - Store body to bucket and store extracted title to DynamoDb Table
-        db_store = post_record(body['title']) # Stores extracted title to DB
+        db_store = post_record(body['title'], table=str(os.environ['TABLE_NAME'])) # Stores extracted title to DB
+
         file_name = create_file_name()  # The name the file is stored under
         bucket_store = save_to_s3(bucket_name=os.environ['BUCKET_NAME'], file_name=file_name, data=response['body'])
+
         if db_store['ResponseMetadata']['HTTPStatusCode'] == 200 and bucket_store['ResponseMetadata']['HTTPStatusCode'] == 200:
             url = get_s3_object_url(bucket_name=os.environ['BUCKET_NAME'], file_name=file_name)
             body = dict(title=json.dumps(body['title']), url=url)
-            return dict(statusCode=200, body=json.dumps(body))
-    return dict(statusCode=200, body=json.dumps(event))
+        return dict(statusCode=200, body=json.dumps(body))
+    # return dict(statusCode=200, body=json.dumps(event))
