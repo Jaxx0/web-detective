beeb5ce615789d7e6beae330b74cee946eaaa3d6
jaxx0 <jackson.kitili@gmail.com>
2019-10-03 16:03:11 +0300

    Refactored docstrings

--- handler.py
+++ handler.py
@@ -1,10 +1,10 @@
 import json
 from files.document_processor import crawler
 
-""" This function receives a URL as an argument from the API gateway and passes it to the crawler function"""
-
 
 def document_crawler(event, context):
+    """ This function receives a URL as an argument from the API gateway and passes it to the crawler function"""
+
     if event['httpMethod'] == 'GET' and event['queryStringParameters']['query']:
         url = event['queryStringParameters']['query']
         body = crawler(url)
