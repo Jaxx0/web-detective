a3fe1a24d66200550a9372ac7951e8592addd33c
jaxx0 <jackson.kitili@gmail.com>
2019-10-03 14:27:00 +0300

    recovered deleted  package json file

--- files/document_processor.py
+++ files/document_processor.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-""" This function crawls a URL and extracts the title from the document"""
-
 
 def crawler(url):
+    """ This function crawls a URL and extracts the title from the document
+    arg url -- the url to crawl """
     try:
         page = requests.get(str(url))
         soup = BeautifulSoup(page.text, 'html.parser')
